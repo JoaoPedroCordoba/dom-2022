7c762a92ec80e5dffd76f7afa9d797bfa217a7f7
jp.8pedro86@gmail.com <jp.8pedro86@gmail.com>
2023-01-03 16:17:01 -0300

    html-Finalizado

---
+++ index.js
@@ -0,0 +1,50 @@
+// const h1 = document.querySelector('h1');
+
+// function exibir(){
+//     console.log('clicou em ', h1);
+// };
+// h1.addEventListener('click', exibir);
+
+// //Retorne o url da pagina atual ultilizando o objeto window
+// const hrefpagina = window.location.href;
+// console.log(hrefpagina);
+
+// //seleciona o primeiro elemento da pagina que possia a classe ativo
+// const ativo = document.querySelectorAll('.ativo')
+// console.log(ativo)
+
+// //retorne a linguagem do navegador
+// const linguagem = window.navigator.language;
+// console.log(linguagem)
+
+// //retorne a largura da janela 
+// const janela = window.innerWidth;
+// console.log(janela)
+
+
+// const animais = document.getElementById('animais');
+// console.log(animais.innerText);
+
+// const gridSection = document.getElementsByClassName('grid-section');
+// console.log(gridSection);
+
+// const primeiraLi = document.querySelector("li");
+// console.log(primeiraLi);
+
+
+// const Animaisimg =  document.querySelectorAll('.animais img');
+// console.log(Animaisimg)
+const imagens =  document.querySelectorAll('img');
+console.log(imagens);
+
+const imagem = document.querySelectorAll('img[src^="imagens/imagem"]');
+console.log(imagem);
+
+const linksInterno =  document.querySelectorAll('[href^="#"]');
+console.log(linksInterno);
+
+const primeiroH2 =  document.querySelector('.animais-descriçao h2');ss
+console.log(primeiroH2);
+
+const ultimoP = document.querySelectorAll('p');
+console.log(ultimoP[ultimoP.length - 1]);22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
